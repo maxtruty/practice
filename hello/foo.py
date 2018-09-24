129c7933e027f2937afdef624cca7c4dd54b6975
Max Truty <maxtruty@Maxs-Air.attlocal.net>
2018-09-23 19:44:27 -0500

    Initial code play.

---
+++ hello/foo.py
@@ -0,0 +1,12 @@
+# This is my first hello world code.
+# This is a comment.
+
+
+def main():
+  # Define the main function.
+  max = "Great!"
+  print(max)
+
+if __name__ == '__main__':
+  # Run main when executed from command line terminal.
+  main()
---
+++ hello/hello.py
@@ -0,0 +1,15 @@
+# This is my first hello world code.
+# This is a comment.
+
+
+def main():
+  # Define the main function.
+  my_string = "Hello World!"
+  print(my_string)
+  print("I like this.")
+  print("Keep it going.")
+
+
+if __name__ == '__main__':
+  # Run main when executed from command line terminal.
+  main()
